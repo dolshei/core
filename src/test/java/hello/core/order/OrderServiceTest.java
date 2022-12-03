5783e6e62e595c6d592d00e0de42dfa186962a3f
LEE.CHANG.HUN <dolshei@gmail.com>
2022-12-03 22:06:41 +0900

    스프링 핵심원리 기본편 예제

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 1000);
+
+        // when
+
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
